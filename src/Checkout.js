68a154d1eff9e471780e837aed68b0275d484911
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-19 22:01:22 -0800

    added subtotal component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import './Checkout.css';
+import React from "react";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
-    return (
-        <div className='checkout'>
-            <div className="checkout__left">
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="Https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_jpg"
+          alt=""
+          className="checkout__ad"
+        />
+      
 
-                <img src="Https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_jpg" alt="" className="checkout__ad" />
-                </div>
-
-                <div>
-                    <h2 className="checkout__title">
-                    Your Shopping Basket</h2>
-                    <CheckoutProduct/>
-                </div>
-                <div className="checkout__right">
-                    
-            </div>
-            
-
-            
-
-           
-
-
+      
+        <h2 className="checkout__title">Your Shopping Basket</h2>
         
-           
-        </div>
-    )
+        <CheckoutProduct />
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -7,8 +7,28 @@ function CheckoutProduct() {
    
             
             <div className="checkoutProduct">
+                <img src="https://res.cloudinary.com/forallpromos/image/fetch/f_auto/https://www.forallgifts.com/sites/default/files/imagecache/585x585/images/products/736457/wedding-tower_1_2_3_1.jpg" 
+                alt="" className="checkoutProduct__image" />
+
              
-           
+           <div className="checkoutProduct__info">
+               <p className="checkoutProduct__title">
+                   Beautiful memory encased in glass for ever
+
+
+               </p>
+
+               <p className="checkoutProduct__price">
+                   <small>$</small>
+                   <strong>65</strong>
+
+               </p>
+               <div className="checkoutProduct__rating">
+                   🌟🌟 
+
+               </div>
+               <button>Remove From Basket</button>
+           </div>
             </div>
      
     )
